010cc06048add2cbb085792037bf89812435387c
BethJW <bethanyjwilliamson16@gmail.com>
2026-01-14 23:17:15 +0000

    Added axios to retrieve weather data from API

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
-function signUp(event) {
+function submitButton(event) {
   event.preventDefault();
   let input = document.querySelector("#search-input");
-  let city = document.querySelector("#city-element");
-  if (input.value.trim() !== "") {
-    city.innerHTML = input.value;
-  } else {
-    city.innerHTML = "No location found";
-  }
+
+  searchCity(input.value);
 }
 let form = document.querySelector("#search-form");
-form.addEventListener("submit", signUp);
+
+function searchCity(city) {
+  let apiKey = "b9b10f3af19o1ba6ec0aed0664cb453t";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(updateWeather);
+}
+
+function updateWeather(response) {
+  let currentTemp = document.querySelector("#current-temp");
+  let temperature = response.data.temperature.current;
+  currentTemp.innerHTML = Math.round(temperature);
+  let city = document.querySelector("#city-element");
+  city.innerHTML = response.data.city;
+  let currentCity = document.querySelector("#current-location");
+  currentCity.innerHTML = response.data.city;
+  city.innerHTML = response.data.city;
+  currentCity.innerHTML = response.data.city;
+}
+form.addEventListener("submit", submitButton);
+searchCity("Crewe");
